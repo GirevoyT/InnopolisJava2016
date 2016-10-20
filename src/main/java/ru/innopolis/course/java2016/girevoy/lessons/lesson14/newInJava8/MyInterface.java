ed48054942869b56ea87ed4df16ada960528e259
t.girevoy <girevoy.t@gmail.com>
2016-10-20 13:01:41 +0300

    Урок 14 Конец урока

---
+++ src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson14/newInJava8/MyInterface.java
@@ -0,0 +1,9 @@
+package ru.innopolis.course.java2016.girevoy.lessons.lesson14.newInJava8;
+
+/**
+ * Created by masterlomaster on 20.10.16.
+ */
+@FunctionalInterface
+public interface MyInterface {
+	void doSome ();
+}
---
+++ src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson14/newInJava8/Test.java
@@ -0,0 +1,10 @@
+package ru.innopolis.course.java2016.girevoy.lessons.lesson14.newInJava8;
+
+/**
+ * Created by masterlomaster on 20.10.16.
+ */
+public class Test {
+	public int doSome (String a,String b) {
+		return a.compareTo(b);
+	}
+}
