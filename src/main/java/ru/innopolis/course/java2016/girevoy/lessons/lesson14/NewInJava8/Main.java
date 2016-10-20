27dc959aef10b3f18a60a2b675e081bd83702f36
t.girevoy <girevoy.t@gmail.com>
2016-10-20 10:24:12 +0300

    Урок 14 Компаратор теперь через лямду

--- src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson14/NewInJava8/Main.java
+++ src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson14/NewInJava8/Main.java
@@ -2,7 +2,6 @@ package ru.innopolis.course.java2016.girevoy.lessons.lesson14.NewInJava8;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -11,12 +10,7 @@ import java.util.List;
 public class Main {
 	public static void main(String[] args) {
 		List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
-		Collections.sort(names, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				return o1.compareTo(o2);
-			}
-		});
+		Collections.sort(names, (String o1, String o2) -> {return o1.compareTo(o2);});
 		for (String name : names) {
 			System.out.println(name);
 		}
