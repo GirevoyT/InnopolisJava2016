26c6a0e91e1b4ae360bd6fc3584330444af85075
t.girevoy.note <girevoy.t@gmail.com>
2016-10-25 11:37:27 +0300

    Урок 17 Новый билдер  (переработанный)

--- src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson17/testBuilder/Computer.java
+++ src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson17/testBuilder/Computer.java
@@ -4,41 +4,38 @@ package ru.innopolis.course.java2016.girevoy.lessons.lesson17.testBuilder;
  * Created by oem on 25.10.16.
  */
 public class Computer {
-	private String motherBoard;
-	private String processor;
-	private String memory;
-	private String os;
+	private final String motherBoard;
+	private final String processor;
+	private final String memory;
+	private final String os;
+
+	private Computer (Builder builder) {
+		this.motherBoard = builder.motherBoard;
+		this.processor = builder.processor;
+		this.memory = builder.memory;
+		this.os = builder.os;
+	}
+
 
 	public String getMotherBoard() {
 		return motherBoard;
 	}
 
-	public void setMotherBoard(String motherBoard) {
-		this.motherBoard = motherBoard;
-	}
+
 
 	public String getProcessor() {
 		return processor;
 	}
 
-	public void setProcessor(String processor) {
-		this.processor = processor;
-	}
 
 	public String getMemory() {
 		return memory;
 	}
 
-	public void setMemory(String memory) {
-		this.memory = memory;
-	}
+
 
 	public String getOs() {
 		return os;
-	}
-
-	public void setOS(String os) {
-		this.os = os;
 	}
 
 	@Override
@@ -46,5 +43,34 @@ public class Computer {
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append(processor).append(" ").append(memory).append(" ").append(os);
 		return stringBuilder.toString();
+	}
+
+	public static class Builder {
+		private String motherBoard;
+		private String processor;
+		private String memory;
+		private String os;
+
+		public Builder withMotherboard( String motherBoard) {
+			this.motherBoard = motherBoard;
+			return this;
+		}
+
+		public Builder withProcessor( String processor) {
+			this.processor = processor;
+			return this;
+		}
+		public Builder withMemory( String memory) {
+			this.memory = memory;
+			return this;
+		}
+		public Builder withOS( String os) {
+			this.os = os;
+			return this;
+		}
+
+		public Computer build() {
+			return new Computer(this);
+		}
 	}
 }
--- src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson17/testBuilder/Main.java
+++ src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson17/testBuilder/Main.java
@@ -5,8 +5,14 @@ package ru.innopolis.course.java2016.girevoy.lessons.lesson17.testBuilder;
  */
 public class Main {
 	public static void main(String[] args) {
-		ComputerBuildManager manager = new ComputerBuildManager(new IntelComputerBuilder());
-		Computer computer = manager.build();
+//		ComputerBuildManager manager = new ComputerBuildManager(new IntelComputerBuilder());
+//		Computer computer = manager.build();
+//		System.out.println(computer);
+		Computer computer = new Computer.Builder()
+				.withMotherboard("1")
+				.withProcessor("2")
+				.withMemory("3")
+				.withOS("4").build();
 		System.out.println(computer);
 	}
 }
