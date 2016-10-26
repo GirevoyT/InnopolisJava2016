dcff4a24cc4c7a0020440e9fe2172f48cc4b7b08
t.girevoy.note <girevoy.t@gmail.com>
2016-10-26 18:50:00 +0300

    UPD

--- src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson18/Main.java
+++ src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson18/Main.java
@@ -7,12 +7,12 @@ import java.sql.*;
  */
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:h2:~/test","sa","");
+        Class.forName("org.postgresql.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/test","postgres","Testarosa123");
         Statement statement = connection.createStatement();
-        ResultSet rs = statement.executeQuery("select * from att");
+        ResultSet rs = statement.executeQuery("select * from public.\"ATT\"");
         while (rs.next()) {
-            System.out.println(rs.getInt("USER_ID") + " " + rs.getInt("LESSON_ID"));
+            System.out.println(rs.getInt("user_id") + " " + rs.getInt("lesson_id"));
         }
         connection.close();
     }
