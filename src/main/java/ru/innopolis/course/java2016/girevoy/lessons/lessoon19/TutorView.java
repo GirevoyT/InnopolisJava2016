fcf7d459b0ecce5ca49f0b44f6945068d75e8b21
T.Girevoy <Girevoy.T@gmail.com>
2016-10-27 13:05:14 +0300

    lesson 19 tmp2

--- src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lessoon19/TutorView.java
+++ src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lessoon19/TutorView.java
@@ -1,11 +1,13 @@
 package ru.innopolis.course.java2016.girevoy.lessons.lessoon19;
 
+import ru.innopolis.course.java2016.girevoy.lessons.lessoon19.viewmodel.StudentViewModel;
+
 /**
  * Created by Girevoy.T on 27.10.2016.
  */
 public class TutorView {
-	Controller controller;
+	StudentViewModel viewModel;
 	Input nameInput = new Input();
 
-	nameInput.setValue(controller.getSrudName);
+	nameInput.setValue(viewModel.getSrudName);
 }
