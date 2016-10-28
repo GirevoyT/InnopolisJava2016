e0ffb5d4237800bf54bca3ef3eeefc874779e43d
T.Girevoy <Girevoy.T@gmail.com>
2016-10-28 09:06:35 +0300

    Закоментирвоал ошибочный (таким и написан) файлик для последующей нормально работы

--- src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lessoon19/TutorView.java
+++ src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lessoon19/TutorView.java
@@ -1,13 +1,13 @@
-package ru.innopolis.course.java2016.girevoy.lessons.lessoon19;
-
-import ru.innopolis.course.java2016.girevoy.lessons.lessoon19.viewmodel.StudentViewModel;
-
-/**
- * Created by Girevoy.T on 27.10.2016.
- */
-public class TutorView {
-	StudentViewModel viewModel;
-	Input nameInput = new Input();
-
-	nameInput.setValue(viewModel.getSrudName);
-}
+//package ru.innopolis.course.java2016.girevoy.lessons.lessoon19;
+//
+//import ru.innopolis.course.java2016.girevoy.lessons.lessoon19.viewmodel.StudentViewModel;
+//
+///**
+// * Created by Girevoy.T on 27.10.2016.
+// */
+//public class TutorView {
+//	StudentViewModel viewModel;
+//	Input nameInput = new Input();
+//
+//	nameInput.setValue(viewModel.getSrudName);
+//}
